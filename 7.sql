9b2d658a7d11b8b3cddb65c2a5736688e8bad9a2
m-maksimenka <maryna-maksimenka@protonmail.com>
2022-01-31 13:24:19 +0300

    correct task description

--- 7.sql
+++ 7.sql
@@ -9,7 +9,9 @@ The Product table contains data on the maker, model number, and type of product 
 Each personal computer in the PC table is unambiguously identified by a unique code, and is additionally characterized by its model (foreign key referring to the Product table), processor speed (in MHz) – speed field, 
 RAM capacity (in Mb) - ram, hard disk drive capacity (in Gb) – hd, CD-ROM speed (e.g, '4x') - cd, and its price. 
 The Laptop table is similar to the PC table, except that instead of the CD-ROM speed, it contains the screen size (in inches) – screen. 
-For each printer model in the Printer table, its output type (‘y’ for color and ‘n’ for monochrome) – color field, printing technology ('Laser', 'Jet', or 'Matrix') – type, and price are specified.   */
+For each printer model in the Printer table, its output type (‘y’ for color and ‘n’ for monochrome) – color field, printing technology ('Laser', 'Jet', or 'Matrix') – type, and price are specified.
+
+Get the models and prices for all commercially available products (of any type) produced by maker B.   */
 
 
 SELECT pc.model, pc.price
