690ccda2333b1ce77bc88b3bd701fee6af257cb3
m-maksimenka <maryna-maksimenka@protonmail.com>
2022-02-03 04:59:38 +0300

    add exercise 18

---
+++ 18.sql
@@ -0,0 +1,25 @@
+--   Exercise: 18
+
+/*  The database scheme consists of four tables:
+Product(maker, model, type)
+PC(code, model, speed, ram, hd, cd, price)
+Laptop(code, model, speed, ram, hd, screen, price)
+Printer(code, model, color, type, price)
+The Product table contains data on the maker, model number, and type of product ('PC', 'Laptop', or 'Printer'). 
+It is assumed that model numbers in the Product table are unique for all makers and product types. Each personal computer in the PC table is unambiguously identified by a unique code, and is additionally characterized by its model (foreign key referring to the Product table), processor speed (in MHz) – 
+speed field, RAM capacity (in Mb) - ram, hard disk drive capacity (in Gb) – hd, CD-ROM speed (e.g, '4x') - cd, and its price. 
+The Laptop table is similar to the PC table, except that instead of the CD-ROM speed, it contains the screen size (in inches) – screen. For each printer model in the Printer table, its output type (‘y’ for color and ‘n’ for monochrome) – color field, printing technology ('Laser', 'Jet', or 'Matrix') – type, and price are specified.
+
+Find the makers of the cheapest color printers.
+Result set: maker, price.   */
+
+
+SELECT DISTINCT product.maker, printer.price
+FROM printer
+JOIN product
+ON printer.model = product.model
+WHERE printer.color = 'y' 
+AND printer.price = (
+SELECT MIN(price) 
+FROM printer 
+WHERE printer.color = 'y');
